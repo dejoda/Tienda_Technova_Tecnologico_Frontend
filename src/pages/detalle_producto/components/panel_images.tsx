e9c9b2f1086030e8b9843a506eddd91385262247
Dejoda <U23216205@utp.edu.pe>
2026-04-16 13:01:07 -0500

    Arreglo en el diseño de detalle_Producto

--- src/pages/detalle_producto/components/panel_images.tsx
+++ src/pages/detalle_producto/components/panel_images.tsx
@@ -1,6 +1,18 @@
 import "./style/panel_images.css";
 import type { ProductoDetalle } from "../../../service/interfaces/ProductoDetalle";
 
+const API_BASE = "http://localhost:8080";
+
+const resolverImagen = (url: string) => {
+  if (!url) return "";
+
+  if (url.startsWith("http://") || url.startsWith("https://") || url.startsWith("data:")) {
+    return url;
+  }
+
+  return `${API_BASE}${url.startsWith("/") ? "" : "/"}${url}`;
+};
+
 interface Props {
   producto: ProductoDetalle;
   imagenPrincipal: string;
@@ -14,15 +26,15 @@ const Panel_Images = ({
 }: Props) => {
   return (
     <div className="detalle-img">
-      <img src={imagenPrincipal} alt={producto.nombre} />
+      <img className="detalle-img-main" src={resolverImagen(imagenPrincipal)} alt={producto.nombre} />
 
       <div className="miniaturas">
         {producto.imagenes.map((img, index) => (
           <img
             key={index}
-            src={img.urlImagen}
+            src={resolverImagen(img.urlImagen)}
             alt="mini"
-            onClick={() => setImagenPrincipal(img.urlImagen)}
+            onClick={() => setImagenPrincipal(resolverImagen(img.urlImagen))}
           />
         ))}
       </div>
